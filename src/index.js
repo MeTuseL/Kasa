1b412cdfc88fc061634d941e0328c47cace3c8d8
MeTuseL <austrie-uly@hotmail.fr>
2024-01-09 21:28:44 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import './styles/index.css'
+import Home from './pages/home/'
+import Survey from './pages/survey/'
+import Header from './components/header'
+import Error from './components/Error'
+import Freelances from './pages/freelances'
+// import Results from './pages/results'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/survey/:questionNumber" element={<Survey />} />
+        <Route path="*" element={<Error />} />
+        <Route path="/freelances" element={<Freelances />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>,
+)
---
+++ src/pages/freelances/index.jsx
@@ -0,0 +1,4 @@
+function Freelances (){
+
+}
+export default Freelances;
