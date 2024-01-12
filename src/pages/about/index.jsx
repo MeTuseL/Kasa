d894bcfc8e4de277c420725be6faeda260c16610
MeTuseL <austrie-uly@hotmail.fr>
2024-01-12 18:44:21 -0400

    feat:add page about with css

---
+++ src/pages/about/index.jsx
@@ -0,0 +1,25 @@
+import Collapse from '../../components/collapse'
+import Banner from '../../components/banner'
+import imgBannerAbout from '../../assets/images/banner_about.png'
+import dataAbout from '../../data/about.json'
+import styles from './css/about.module.css'
+
+function About() {
+  return (
+    <section>
+      <Banner picture={imgBannerAbout} />
+
+      <div className={styles.container}>
+        {dataAbout.map((data, index) => (
+          <Collapse
+            key={`${data.title}-${index}`}
+            title={data.title}
+            content={data.description}
+          />
+        ))}
+      </div>
+    </section>
+  )
+}
+
+export default About
