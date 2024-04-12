57ac5d11a602a585a7d768144542a8fae2419151
MeTuseL <austrie-uly@hotmail.fr>
2024-04-12 02:44:31 -0400

    feat: Configure GitHub Pages

--- src/index.jsx
+++ src/index.jsx
@@ -17,7 +17,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/Kasa">
       <div className={styles.container}>
         {/*add a div for center body */}
         <Header />
